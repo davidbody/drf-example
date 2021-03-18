28ec35e93ea5b71c7e65c826359ca8c987c5621e
David W. Body <davidbody@bigcreek.com>
2021-03-18 11:49:51 -0500

    Install REST framework app and set default permissions

--- drf_example/settings.py
+++ drf_example/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'drf_example.my_api',
 ]
 
@@ -79,6 +80,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ]
 }
 
 AUTH_USER_MODEL = "my_api.User"
