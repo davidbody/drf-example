a25abc871b3ea167042f74b2f511cacf75e61f50
David W. Body <davidbody@bigcreek.com>
2021-03-18 15:37:30 -0500

    Total views endpoint and unit test

--- drf_example/my_api/tests.py
+++ drf_example/my_api/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class ViewsTest(APITestCase):
+    def test_get_total_views(self):
+        """
+        Get total video views
+        """
+        url = reverse('total-views')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {'total_views': 123456789})
--- drf_example/my_api/views.py
+++ drf_example/my_api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class TotalViewsView(APIView):
+    """
+    Returns total number of video views across all platforms
+    """
+    def get(self, request):
+        return Response({'total_views': 123456789} )
